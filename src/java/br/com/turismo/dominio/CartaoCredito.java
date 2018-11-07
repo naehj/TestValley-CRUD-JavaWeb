d58243a1ac073d9d2a178a62201fe4ff3e548edf
Jhean Marllos Gonçalves Sombra <jheanmarllos1@gmail.com>
2018-11-07 20:41:26 -0300

    Alterando cartão de crédito no domínio para refletir o que está na descrição de requisitos

--- src/java/br/com/turismo/dominio/CartaoCredito.java
+++ src/java/br/com/turismo/dominio/CartaoCredito.java
@@ -9,19 +9,13 @@ package br.com.turismo.dominio;
  *
  * @author hisak
  */
-
 public class CartaoCredito extends EntidadeDominio {
 
     private String numero;
-    private String mes;
-    private String ano;
     private String codigo;
     private String nome;
     private String bandeira;
-    private boolean status;
-    private boolean preferencia;
-    
- 
+
     private int cliente;
 
     public String getNumero() {
@@ -30,22 +24,6 @@ public class CartaoCredito extends EntidadeDominio {
 
     public void setNumero(String numero) {
         this.numero = numero;
-    }
-
-    public String getMes() {
-        return mes;
-    }
-
-    public void setMes(String mes) {
-        this.mes = mes;
-    }
-
-    public String getAno() {
-        return ano;
-    }
-
-    public void setAno(String ano) {
-        this.ano = ano;
     }
 
     public String getCodigo() {
@@ -72,30 +50,12 @@ public class CartaoCredito extends EntidadeDominio {
         this.cliente = cliente;
     }
 
-
-
     public String getBandeira() {
         return bandeira;
     }
 
     public void setBandeira(String bandeira) {
         this.bandeira = bandeira;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    public boolean isPreferencia() {
-        return preferencia;
-    }
-
-    public void setPreferencia(boolean preferencia) {
-        this.preferencia = preferencia;
     }
 
 }
