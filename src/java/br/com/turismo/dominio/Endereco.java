265ba2a8a5f1461e074e2de0a24f8bb2aee6a02a
Jhean Marllos Gonçalves Sombra <jheanmarllos1@gmail.com>
2018-11-10 09:40:45 -0300

    Refatorando a classe de Endereço para adicionar comentário em cada método e adicionar as variáveis responsáveis pelo tipo de logradou e de residência.

--- src/java/br/com/turismo/dominio/Endereco.java
+++ src/java/br/com/turismo/dominio/Endereco.java
@@ -10,7 +10,6 @@ package br.com.turismo.dominio;
  * @author hisak
  */
 public class Endereco extends EntidadeDominio {
-
     private String logradouro;
     private String numero;
     private String complemento;
@@ -19,62 +18,133 @@ public class Endereco extends EntidadeDominio {
     private String cidade;
     private String estado;
     private String pais;
+    private String tipoLogradouro;
+    private String tipoResidencia;
 
+    /**
+     * @return the logradouro
+     */
     public String getLogradouro() {
         return logradouro;
     }
 
+    /**
+     * @param logradouro the logradouro to set
+     */
     public void setLogradouro(String logradouro) {
         this.logradouro = logradouro;
     }
 
+    /**
+     * @return the numero
+     */
     public String getNumero() {
         return numero;
     }
 
+    /**
+     * @param numero the numero to set
+     */
     public void setNumero(String numero) {
         this.numero = numero;
     }
 
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
+    /**
+     * @return the complemento
+     */
     public String getComplemento() {
         return complemento;
     }
 
+    /**
+     * @param complemento the complemento to set
+     */
     public void setComplemento(String complemento) {
         this.complemento = complemento;
     }
 
+    /**
+     * @return the cep
+     */
     public String getCep() {
         return cep;
     }
 
+    /**
+     * @param cep the cep to set
+     */
     public void setCep(String cep) {
         this.cep = cep;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the pais
+     */
+    public String getPais() {
+        return pais;
+    }
+
+    /**
+     * @param pais the pais to set
+     */
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    
+    /**
+     * @return the tipoLogradouro
+     */
+    public String getTipoLogradouro() {
+        return tipoLogradouro;
+    }
+
+    /**
+     * @param tipoLogradouro the tipoLogradouro to set
+     */
+    public void setTipoLogradouro(String tipoLogradouro) {
+        this.tipoLogradouro = tipoLogradouro;
+    }
+
+    /**
+     * @return the tipoResidencia
+     */
+    public String getTipoResidencia() {
+        return tipoResidencia;
+    }
+
+    /**
+     * @param tipoResidencia the tipoResidencia to set
+     */
+    public void setTipoResidencia(String tipoResidencia) {
+        this.tipoResidencia = tipoResidencia;
     }
 
 }
