1ec55bd2be4746c46574d3231fdaaf41c3c67c22
Jhean Marllos Gonçalves Sombra <jheanmarllos1@gmail.com>
2018-11-16 23:59:38 -0300

    View Helper de Cartão de crédito com os métodos de salvar e deletar cartão já funcionando

--- src/java/br/com/turismo/web/vh/CartaoCreditoViewHelper.java
+++ src/java/br/com/turismo/web/vh/CartaoCreditoViewHelper.java
@@ -9,8 +9,14 @@ import br.com.turismo.core.fachada.Resultado;
 import br.com.turismo.dominio.CartaoCredito;
 import br.com.turismo.dominio.Cliente;
 import br.com.turismo.dominio.EntidadeDominio;
+import br.com.turismo.web.command.AtualizarCommand;
 import br.com.turismo.web.command.ConsultarCommand;
+import br.com.turismo.web.command.ExcluirCommand;
+import br.com.turismo.web.command.SalvarCommand;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -24,96 +30,143 @@ public class CartaoCreditoViewHelper implements IViewHelper {
 
     //Objetos necessários para cadastrar um Cliente
     Cliente cliente;
-
+    
     CartaoCredito cartao;
-
+    
     @Override
     public EntidadeDominio getEntidade(HttpServletRequest request) {
 
         //Recebe operação do formulário, na request.
         String operacao = request.getParameter("operacao");
-
+        
         if (operacao.equals("SALVAR")) {
-
+            cliente = new Cliente();
+            cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
             cartao = new CartaoCredito();
-            String idCliente = request.getParameter("idCliente");
-            String mes = request.getParameter("mesCartao");
             String numero = request.getParameter("numeroCartao");
-            String ano = request.getParameter("anoCartao");
             String codigo = request.getParameter("codigoCartao");
             String bandeira = request.getParameter("bandeira");
             String nome = request.getParameter("nomeCartao");
-            String status = request.getParameter("statusCartao");
-            String preferencia = request.getParameter("preferencia");
-
-            if (idCliente != null && !idCliente.trim().equals("")) {
-                cartao.setCliente(Integer.parseInt(idCliente));
-            }
             if (numero != null && !numero.trim().equals("")) {
                 cartao.setNumero(numero);
             }
-
-            if (mes != null && !mes.trim().equals("")) {
-                cartao.setMes(mes);
-            }
-
-            if (ano != null && !ano.trim().equals("")) {
-                cartao.setAno(ano);
-            }
-
+            
             if (codigo != null && !codigo.trim().equals("")) {
                 cartao.setCodigo(codigo);
             }
-
+            
             if (bandeira != null && !bandeira.trim().equals("")) {
                 cartao.setBandeira(bandeira);
             }
             if (nome != null && !nome.trim().equals("")) {
                 cartao.setNome(nome);
             }
-            /*  if (status != null && status.trim().equals("")) {
-                cartao.setBandeira(boolean.bandeira);
-            }
-            if (preferencia != null && preferencia.trim().equals("")) {
-                cartao.setBandeira(bandeira);
-            }*/
-
-            return cartao;
-
+            HashSet<CartaoCredito> cartoes = new HashSet<>();
+            cartoes.add(cartao);
+            cliente.setCartaoCredito(cartoes);
+            return cliente;
+            
         }
+        
+        if (operacao.equals("ATUALIZAR")) {
+            
+            cartao = new CartaoCredito();
+            String numero = request.getParameter("numeroCartao");
+            String codigo = request.getParameter("codigoCartao");
+            String bandeira = request.getParameter("bandeira");
+            String nome = request.getParameter("nomeCartao");
+            
+            cartao.setId(Integer.parseInt(request.getParameter("idCartao")));
+            
+            if (numero != null && !numero.trim().equals("")) {
+                cartao.setNumero(numero);
+            }
+            
+            if (codigo != null && !codigo.trim().equals("")) {
+                cartao.setCodigo(codigo);
+            }
+            
+            if (bandeira != null && !bandeira.trim().equals("")) {
+                cartao.setBandeira(bandeira);
+            }
+            if (nome != null && !nome.trim().equals("")) {
+                cartao.setNome(nome);
+            }
+            
+            return cartao;
+            
+        }
+        
+        if (operacao.equals("EXCLUIR")) {
+            
+            cartao = new CartaoCredito();
+            
+            cartao.setId(Integer.parseInt(request.getParameter("idCartao")));
+            
+            return cartao;
+            
+        }
+        
         return cartao;
     }
-
+    
     @Override
     public void setView(Resultado resultado, HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException {
-
+        
         RequestDispatcher rd = null;
-
+        
         String operacao = request.getParameter("operacao");
-
+        
         if (operacao.equals("SALVAR")) {
             if (resultado.getMsg() != null) {
-
+                
                 request.getSession().setAttribute("mensagem", resultado.getMsg());
-
+                
                 rd = request.getRequestDispatcher("cadastroCartao.jsp");
             } else {
-                ConsultarCommand consultar = new ConsultarCommand();
+                ConsultarCommand consulta = new ConsultarCommand();
                 cliente = new Cliente();
-                cartao = new CartaoCredito();
-                cartao = (CartaoCredito) resultado.getEntidades().get(0);
-                cliente.setId(cartao.getCliente());
-                System.out.println("cliente id" + cliente.getId());
-                br.com.turismo.core.fachada.Resultado result = consultar.execute(cliente);
-                request.getSession().setAttribute("resultado", result.getEntidades());
-
+                cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
+                resultado = consulta.execute(cliente);
+                request.getSession().setAttribute("resultado", resultado.getEntidades());
+                
                 rd = request.getRequestDispatcher("paginaCliente.jsp");
             }
         }
-
+        
+        if (operacao.equals("ATUALIZAR")) {
+            if (resultado.getMsg() != null) {
+                
+                request.getSession().setAttribute("mensagem", resultado.getMsg());
+                
+                rd = request.getRequestDispatcher("atualizarCartao.jsp");
+            } else {
+                request.getSession().setAttribute("resultado", resultado.getEntidades());
+                
+                rd = request.getRequestDispatcher("paginaCliente.jsp");
+            }
+        }
+        
+        if (operacao.equals("EXCLUIR")) {
+            if (resultado.getMsg() != null) {
+                
+                request.getSession().setAttribute("mensagem", resultado.getMsg());
+                
+                rd = request.getRequestDispatcher("paginaInicial.jsp");
+            } else {
+                ConsultarCommand consulta = new ConsultarCommand();
+                cliente = new Cliente();
+                cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
+                resultado = consulta.execute(cliente);
+                request.getSession().setAttribute("resultado", resultado.getEntidades());
+                
+                rd = request.getRequestDispatcher("paginaCliente.jsp");
+            }
+        }
+        
         rd.forward(request, response);
     }
-
+    
 }
