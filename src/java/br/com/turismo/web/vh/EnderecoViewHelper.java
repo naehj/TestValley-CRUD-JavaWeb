4b145beec58ce7d04c64e726216826c6f78e3381
Jhean Marllos Gonçalves Sombra <jheanmarllos1@gmail.com>
2018-11-18 17:15:45 -0300

    EnderecoViewHelper completo e funcional

--- src/java/br/com/turismo/web/vh/EnderecoViewHelper.java
+++ src/java/br/com/turismo/web/vh/EnderecoViewHelper.java
@@ -6,15 +6,18 @@
 package br.com.turismo.web.vh;
 
 import br.com.turismo.core.fachada.Resultado;
-import br.com.turismo.dominio.Cidade;
 import br.com.turismo.dominio.Cliente;
 import br.com.turismo.dominio.Endereco;
 import br.com.turismo.dominio.EntidadeDominio;
-import br.com.turismo.dominio.Estado;
-import br.com.turismo.dominio.Pais;
+import br.com.turismo.web.command.AtualizarCommand;
+import br.com.turismo.web.command.ConsultarCommand;
+import br.com.turismo.web.command.ExcluirCommand;
+import br.com.turismo.web.command.SalvarCommand;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,8 +30,7 @@ public class EnderecoViewHelper implements IViewHelper {
 
     //Objetos necessários para cadastrar um Cliente
     Endereco endereco;
-
-    
+    Cliente cliente;
 
     @Override
     public EntidadeDominio getEntidade(HttpServletRequest request) {
@@ -37,17 +39,20 @@ public class EnderecoViewHelper implements IViewHelper {
         String operacao = request.getParameter("operacao");
 
         if (operacao.equals("SALVAR")) {
+            cliente = new Cliente();
+            cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
             endereco = new Endereco();
-           
 
             String logradouro = request.getParameter("logradouro");
             String numero = request.getParameter("numeroRes");
-            String bairro = request.getParameter("bairro");
             String complemento = request.getParameter("complemento");
-            String idCidade = request.getParameter("idCidade");
-            String idEstado = request.getParameter("idEstado");
-            String idPais = request.getParameter("idPais");
+            String cidade = request.getParameter("cidade");
+            String estado = request.getParameter("estado");
+            String pais = request.getParameter("pais");
             String cep = request.getParameter("cep");
+            String tipoLogradouro = request.getParameter("tipo_logradouro");
+            String tipoResidencia = request.getParameter("tipo_logradouro");
+
             if (logradouro != null && !logradouro.trim().equals("")) {
                 endereco.setLogradouro(logradouro);
             }
@@ -56,43 +61,193 @@ public class EnderecoViewHelper implements IViewHelper {
                 endereco.setNumero(numero);
             }
 
-            if (bairro != null && !bairro.trim().equals("")) {
-                endereco.setBairro(bairro);
+            if (complemento != null && !complemento.trim().equals("")) {
+                endereco.setComplemento(complemento);
+            }
+
+            if (cidade != null && !cidade.trim().equals("")) {
+                endereco.setCidade(cidade);
+            }
+
+            if (estado != null && !estado.trim().equals("")) {
+                endereco.setEstado(estado);
+            }
+            if (pais != null && !pais.trim().equals("")) {
+                endereco.setPais(pais);
+            }
+            if (cep != null && !cep.trim().equals("")) {
+                endereco.setCep(cep);
+            }
+            if (tipoLogradouro != null && !tipoLogradouro.trim().equals("")) {
+                endereco.setTipoLogradouro(tipoLogradouro);
+            }
+            if (tipoResidencia != null && !tipoResidencia.trim().equals("")) {
+                endereco.setTipoResidencia(tipoResidencia);
+            }
+            HashSet<Endereco> enderecos = new HashSet<>();
+            enderecos.add(endereco);
+            cliente.setEnd_De_Entrega(enderecos);
+
+            return cliente;
+        }
+
+        if (operacao.equals("ATUALIZAR")) {
+            endereco = new Endereco();
+
+            String logradouro = request.getParameter("logradouro");
+            String numero = request.getParameter("numeroRes");
+            String complemento = request.getParameter("complemento");
+            String cidade = request.getParameter("cidade");
+            String estado = request.getParameter("estado");
+            String pais = request.getParameter("pais");
+            String cep = request.getParameter("cep");
+            String tipoLogradouro = request.getParameter("tipo_logradouro");
+            String tipoResidencia = request.getParameter("tipo_logradouro");
+
+            endereco.setId(Integer.parseInt(request.getParameter("idEndereco")));
+
+            if (logradouro != null && !logradouro.trim().equals("")) {
+                endereco.setLogradouro(logradouro);
+            }
+
+            if (numero != null && !numero.trim().equals("")) {
+                endereco.setNumero(numero);
             }
 
             if (complemento != null && !complemento.trim().equals("")) {
                 endereco.setComplemento(complemento);
             }
 
-            if (idCidade != null && !idCidade.trim().equals("")) {
-                endereco.setCidade(new Cidade());
-                endereco.getCidade().setId(Integer.parseInt(idCidade));
+            if (cidade != null && !cidade.trim().equals("")) {
+                endereco.setCidade(cidade);
             }
 
-            if (idEstado != null && !idEstado.trim().equals("")) {
-                endereco.getCidade().setEstado(new Estado());
-                endereco.getCidade().getEstado().setId(Integer.parseInt(idEstado));
+            if (estado != null && !estado.trim().equals("")) {
+                endereco.setEstado(estado);
             }
-            if (idPais != null && !idPais.trim().equals("")) {
-                endereco.getCidade().getEstado().setPais(new Pais());
-                endereco.getCidade().getEstado().getPais().setId(Integer.parseInt(idPais));
+            if (pais != null && !pais.trim().equals("")) {
+                endereco.setPais(pais);
             }
-            if (cep != null && cep.trim().equals("")) {
-                endereco.getCep();
+            if (cep != null && !cep.trim().equals("")) {
+                endereco.setCep(cep);
             }
+            if (tipoLogradouro != null && !tipoLogradouro.trim().equals("")) {
+                endereco.setTipoLogradouro(tipoLogradouro);
+            }
+            if (tipoResidencia != null && !tipoResidencia.trim().equals("")) {
+                endereco.setTipoResidencia(tipoResidencia);
+            }
+            return endereco;
+        }
+
+        if (operacao.equals("PREATUALIZAR")) {
+            cliente = new Cliente();
+            cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
+            if (request.getRequestURI().contains("Entrega")) {
+                endereco = new Endereco();
+                endereco.setId(Integer.parseInt(request.getParameter("idEndereco")));
+
+                return endereco;
+            }
+            return cliente;
+        }
+
+        if (operacao.equals("EXCLUIR")) {
+            endereco = new Endereco();
+            endereco.setId(Integer.parseInt(request.getParameter("idEndereco")));
 
             return endereco;
         }
 
-      
         return endereco;
     }
 
     @Override
     public void setView(Resultado resultado, HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        RequestDispatcher rd = null;
 
-       
+        String operacao = request.getParameter("operacao");
+
+        if (operacao.equals("SALVAR")) {
+            if (resultado.getMsg() != null) {
+
+                request.getSession().setAttribute("mensagem", resultado.getMsg());
+
+                rd = request.getRequestDispatcher("cadastroEndereco.jsp");
+            } else {
+                ConsultarCommand consulta = new ConsultarCommand();
+                cliente = new Cliente();
+                cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
+                resultado = consulta.execute(cliente);
+                request.getSession().setAttribute("resultado", resultado.getEntidades());
+                rd = request.getRequestDispatcher("listarEnderecos.jsp");
+            }
+        }
+
+        if (operacao.equals("ATUALIZAR")) {
+            if (resultado.getMsg() != null) {
+
+                request.getSession().setAttribute("mensagem", resultado.getMsg());
+
+                rd = request.getRequestDispatcher("atualizarCartao.jsp");
+            } else {
+                ConsultarCommand consulta = new ConsultarCommand();
+                cliente = new Cliente();
+                cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
+                resultado = consulta.execute(cliente);
+                request.getSession().setAttribute("resultado", resultado.getEntidades());
+                if (request.getRequestURI().contains("Cobranca")) {
+                    rd = request.getRequestDispatcher("paginaCliente.jsp");
+                } else if (request.getRequestURI().contains("Entrega")) {
+                    rd = request.getRequestDispatcher("listarEnderecos.jsp");
+                }
+            }
+        }
+
+        if (operacao.equals("PREATUALIZAR")) {
+            if (resultado.getMsg() != null) {
+
+                request.getSession().setAttribute("mensagem", resultado.getMsg());
+
+                rd = request.getRequestDispatcher("paginaInicial.jsp");
+            } else {
+
+                if (request.getRequestURI().contains("Cobranca")) {
+                    request.getSession().setAttribute("resultado", resultado.getEntidades());
+                    rd = request.getRequestDispatcher("atualizarCobranca.jsp");
+                } else if (request.getRequestURI().contains("Entrega")) {
+                    cliente = new Cliente();
+                    cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
+                    List<EntidadeDominio> entidades = new ArrayList<>();
+                    entidades.add(resultado.getEntidade());
+                    entidades.add(cliente);
+                    resultado.setEntidades(entidades);
+                    request.getSession().setAttribute("resultado", resultado.getEntidades());
+                    rd = request.getRequestDispatcher("atualizarEntrega.jsp");
+                }
+
+            }
+        }
+
+        if (operacao.equals("EXCLUIR")) {
+            if (resultado.getMsg() != null) {
+
+                request.getSession().setAttribute("mensagem", resultado.getMsg());
+
+                rd = request.getRequestDispatcher("paginaInicial.jsp");
+            } else {
+                ConsultarCommand consulta = new ConsultarCommand();
+                cliente = new Cliente();
+                cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
+                resultado = consulta.execute(cliente);
+                request.getSession().setAttribute("resultado", resultado.getEntidades());
+
+                rd = request.getRequestDispatcher("listarEnderecos.jsp");
+            }
+        }
+
+        rd.forward(request, response);
     }
 
 }
