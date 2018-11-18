e52c91c70e228fd094498b8cdd617ae45b96edf0
Jhean Marllos Gonçalves Sombra <jheanmarllos1@gmail.com>
2018-11-18 11:02:19 -0300

    View Helper de cartão de crédito completa

--- src/java/br/com/turismo/web/vh/CartaoCreditoViewHelper.java
+++ src/java/br/com/turismo/web/vh/CartaoCreditoViewHelper.java
@@ -30,15 +30,15 @@ public class CartaoCreditoViewHelper implements IViewHelper {
 
     //Objetos necessários para cadastrar um Cliente
     Cliente cliente;
-    
+
     CartaoCredito cartao;
-    
+
     @Override
     public EntidadeDominio getEntidade(HttpServletRequest request) {
 
         //Recebe operação do formulário, na request.
         String operacao = request.getParameter("operacao");
-        
+
         if (operacao.equals("SALVAR")) {
             cliente = new Cliente();
             cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
@@ -50,11 +50,11 @@ public class CartaoCreditoViewHelper implements IViewHelper {
             if (numero != null && !numero.trim().equals("")) {
                 cartao.setNumero(numero);
             }
-            
+
             if (codigo != null && !codigo.trim().equals("")) {
                 cartao.setCodigo(codigo);
             }
-            
+
             if (bandeira != null && !bandeira.trim().equals("")) {
                 cartao.setBandeira(bandeira);
             }
@@ -65,65 +65,75 @@ public class CartaoCreditoViewHelper implements IViewHelper {
             cartoes.add(cartao);
             cliente.setCartaoCredito(cartoes);
             return cliente;
-            
+
         }
-        
+
         if (operacao.equals("ATUALIZAR")) {
-            
+
             cartao = new CartaoCredito();
             String numero = request.getParameter("numeroCartao");
             String codigo = request.getParameter("codigoCartao");
             String bandeira = request.getParameter("bandeira");
             String nome = request.getParameter("nomeCartao");
-            
+
             cartao.setId(Integer.parseInt(request.getParameter("idCartao")));
-            
+
             if (numero != null && !numero.trim().equals("")) {
                 cartao.setNumero(numero);
             }
-            
+
             if (codigo != null && !codigo.trim().equals("")) {
                 cartao.setCodigo(codigo);
             }
-            
+
             if (bandeira != null && !bandeira.trim().equals("")) {
                 cartao.setBandeira(bandeira);
             }
             if (nome != null && !nome.trim().equals("")) {
                 cartao.setNome(nome);
             }
-            
+
             return cartao;
-            
+
         }
-        
-        if (operacao.equals("EXCLUIR")) {
-            
+
+        if (operacao.equals("PREATUALIZAR")) {
             cartao = new CartaoCredito();
-            
-            cartao.setId(Integer.parseInt(request.getParameter("idCartao")));
-            
+            String id = request.getParameter("idCartao");
+            if (id != null && !id.trim().equals("")) {
+                cartao.setId(Integer.parseInt(id));
+                return cartao;
+            }
             return cartao;
-            
         }
-        
+
+        if (operacao.equals("EXCLUIR")) {
+
+            cartao = new CartaoCredito();
+
+            cartao.setId(Integer.parseInt(request.getParameter("idCartao")));
+
+            return cartao;
+
+        }
+
         return cartao;
     }
-    
+
     @Override
     public void setView(Resultado resultado, HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         RequestDispatcher rd = null;
-        
+
         String operacao = request.getParameter("operacao");
-        
+
         if (operacao.equals("SALVAR")) {
             if (resultado.getMsg() != null) {
-                
+
                 request.getSession().setAttribute("mensagem", resultado.getMsg());
-                
+
                 rd = request.getRequestDispatcher("cadastroCartao.jsp");
             } else {
                 ConsultarCommand consulta = new ConsultarCommand();
@@ -131,29 +141,48 @@ public class CartaoCreditoViewHelper implements IViewHelper {
                 cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
                 resultado = consulta.execute(cliente);
                 request.getSession().setAttribute("resultado", resultado.getEntidades());
-                
-                rd = request.getRequestDispatcher("paginaCliente.jsp");
+
+                rd = request.getRequestDispatcher("listarCartoes.jsp");
             }
         }
-        
+
         if (operacao.equals("ATUALIZAR")) {
             if (resultado.getMsg() != null) {
-                
+
                 request.getSession().setAttribute("mensagem", resultado.getMsg());
-                
+
                 rd = request.getRequestDispatcher("atualizarCartao.jsp");
             } else {
+                ConsultarCommand consulta = new ConsultarCommand();
+                cliente = new Cliente();
+                cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
+                resultado = consulta.execute(cliente);
                 request.getSession().setAttribute("resultado", resultado.getEntidades());
-                
-                rd = request.getRequestDispatcher("paginaCliente.jsp");
+
+                rd = request.getRequestDispatcher("listarCartoes.jsp");
             }
         }
         
+        if (operacao.equals("PREATUALIZAR")) {
+            if (resultado.getMsg() != null) {
+
+                request.getSession().setAttribute("mensagem", resultado.getMsg());
+                rd = request.getRequestDispatcher("listarCartoes.jsp");
+            } else {
+                cliente = new Cliente();
+                cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
+                resultado.getEntidades().add(cliente);
+                request.getSession().setAttribute("resultado", resultado.getEntidades());
+
+                rd = request.getRequestDispatcher("atualizarCartao.jsp");
+            }
+        }
+
         if (operacao.equals("EXCLUIR")) {
             if (resultado.getMsg() != null) {
-                
+
                 request.getSession().setAttribute("mensagem", resultado.getMsg());
-                
+
                 rd = request.getRequestDispatcher("paginaInicial.jsp");
             } else {
                 ConsultarCommand consulta = new ConsultarCommand();
@@ -161,12 +190,12 @@ public class CartaoCreditoViewHelper implements IViewHelper {
                 cliente.setId(Integer.parseInt(request.getParameter("idCliente")));
                 resultado = consulta.execute(cliente);
                 request.getSession().setAttribute("resultado", resultado.getEntidades());
-                
-                rd = request.getRequestDispatcher("paginaCliente.jsp");
+
+                rd = request.getRequestDispatcher("listarCartoes.jsp");
             }
         }
-        
+
         rd.forward(request, response);
     }
-    
+
 }
